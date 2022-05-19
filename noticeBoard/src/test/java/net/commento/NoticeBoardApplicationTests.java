8480fab87e0a5731092f3a2f1660e49bac058890
bluemudd <parkjm0817@gmail.com>
2022-05-19 12:12:51 +0900

    first commento commit

---
+++ noticeBoard/src/test/java/net/commento/NoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package net.commento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
